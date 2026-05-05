3f09a8da5c8dcbd99c83d3d4bf5d1eedb8d101d9
Amos Olamide <olamideamos3@gmail.com>
2026-05-05 15:19:17 +0100

    Create render function

--- todo.js
+++ todo.js
@@ -13,3 +13,34 @@ function saveTasks() {
   localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
+function renderTasks() {
+  taskList.innerHTML = '';
+  let filteredTasks = tasks;
+  if (currentFilter === 'active') {
+    filteredTasks = tasks.filter(task => !task.completed);
+  } else if (currentFilter === 'completed') {
+    filteredTasks = tasks.filter(task => task.completed);
+  } 
+
+  filteredTasks.forEach((task) => {
+    const li = document.createElement('li');
+    li.classList.add('task-item');
+    if (task.completed) {
+      li.classList.add('completed');
+    }
+    li.innerHTML = `
+      <span>${task.title}</span>
+      <div>
+        <button onclick="toggleTask(${task.id})">
+          ${task.completed ? 'Undo' : 'completed'}
+        </button>
+
+        <button onclick="deleteTask(${task.id})">
+          Delete
+        </button>
+      </div>
+    `;
+
+    taskList.appendChild(li);
+  });
+}
