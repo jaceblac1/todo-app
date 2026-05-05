8662b8ae000b6957f79e4878b03383a4c968b8ea
Amos Olamide <olamideamos3@gmail.com>
2026-05-05 16:54:27 +0100

    Updated delete and toggle function

--- todo.js
+++ todo.js
@@ -1,38 +1,40 @@
-const taskInput = document.getElementById('taskInput');
-const addBtn = document.getElementById('addBtn');
-const taskList = document.getElementById('taskList');
-const filterButtons = document.querySelectorAll('.filters button')
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+const filterButtons = document.querySelectorAll(".filters button");
 
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-
-let currentFilter = 'all';
-
+let currentFilter = "all";
 
 function saveTasks() {
-  localStorage.setItem('tasks', JSON.stringify(tasks))
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 function renderTasks() {
-  taskList.innerHTML = '';
+  taskList.innerHTML = "";
   let filteredTasks = tasks;
-  if (currentFilter === 'active') {
-    filteredTasks = tasks.filter(task => !task.completed);
-  } else if (currentFilter === 'completed') {
-    filteredTasks = tasks.filter(task => task.completed);
-  } 
+  if (currentFilter === "active") {
+    filteredTasks = tasks.filter((task) => {
+      return !task.completed;
+    });
+  } else if (currentFilter === "completed") {
+    filteredTasks = tasks.filter((task) => {
+      return task.completed;
+    });
+  }
 
   filteredTasks.forEach((task) => {
-    const li = document.createElement('li');
-    li.classList.add('task-item');
+    const li = document.createElement("li");
+    li.classList.add("task-item");
     if (task.completed) {
-      li.classList.add('completed');
+      li.classList.add("completed");
     }
     li.innerHTML = `
       <span>${task.title}</span>
       <div>
         <button onclick="toggleTask(${task.id})">
-          ${task.completed ? 'Undo' : 'completed'}
+          ${task.completed ? "Undo" : "completed"}
         </button>
 
         <button onclick="deleteTask(${task.id})">
@@ -48,8 +50,8 @@ function renderTasks() {
 function addTask() {
   const title = taskInput.value.trim();
 
-  if (title === '') {
-    alert('Task cannot be empty');
+  if (title === "") {
+    alert("Task cannot be empty");
     return;
   }
 
@@ -57,17 +59,17 @@ function addTask() {
     id: Date.now(),
     title: title,
     completed: false,
-    createdAt: new Date().toISOString()
+    createdAt: new Date().toISOString(),
   };
 
   tasks.push(newTask);
   saveTasks();
   renderTasks();
-  taskInput.value = '';
+  taskInput.value = "";
 }
 
 function toggleTask(id) {
-  task.forEach((task) => {
+  tasks.forEach((task) => {
     if (task.id === id) {
       task.completed = !task.completed;
     }
@@ -77,32 +79,30 @@ function toggleTask(id) {
   renderTasks();
 }
 
-
 function deleteTask(id) {
   tasks = tasks.filter((task) => {
-    task.id !==id;
+    return task.id !== id;
   });
-
   saveTasks();
   renderTasks();
 }
 
-addBtn.addEventListener('click', () => {
+addBtn.addEventListener("click", () => {
   addTask();
 });
 
-taskInput.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
+taskInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
     addTask();
   }
 });
 
 filterButtons.forEach((button) => {
-  button.addEventListener('click', () => {
+  button.addEventListener("click", () => {
     currentFilter = button.dataset.filter;
 
     renderTasks();
-    });
+  });
 });
 
 renderTasks();
